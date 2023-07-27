264c54d662608a5a5b0bb5c6098979bbc16ad13e
Kurt Portelli <kurt.portelli@redacreltd.com>
2023-07-27 12:38:23 +0200

    allow all out rds

--- aws/rds/main.tf
+++ aws/rds/main.tf
@@ -71,3 +71,11 @@ resource "aws_security_group_rule" "access_from_vpc" {
 
   cidr_blocks = [var.vpc.cidr]
 }
+
+resource "aws_security_group_rule" "allow_all_out" {
+  type              = "egress"
+  to_port           = 0
+  protocol          = "-1"
+  from_port         = 0
+  security_group_id = aws_security_group.this.id
+}
