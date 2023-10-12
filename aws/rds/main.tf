c0e55044891603c35481cfb1fc38d8e7446715b4
Akhil Anil <140489780+akhil-redacre@users.noreply.github.com>
2023-10-12 18:59:57 +0530

    removed the 2nd max_allocated_size variable from RDS

--- aws/rds/main.tf
+++ aws/rds/main.tf
@@ -14,7 +14,6 @@ resource "aws_db_instance" "this" {
   identifier            = local.db_identifier
   db_name               = local.db_name
   allocated_storage     = var.allocated_storage
-  max_allocated_storage = var.max_allocated_storage
   instance_class        = var.instance_class
   engine_version        = var.engine_version
   tags                  = var.tags
