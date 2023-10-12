8984421ff906daa63a987adbd9f53b3dd8e7a2ec
Akhil Anil <140489780+akhil-redacre@users.noreply.github.com>
2023-10-12 18:12:55 +0530

    removed aws_security_group_rule" "allow_all_out" and blue_green_update

--- aws/rds/main.tf
+++ aws/rds/main.tf
@@ -49,10 +49,6 @@ resource "aws_db_instance" "this" {
 
   max_allocated_storage = var.max_allocated_storage_factor * var.allocated_storage
 
-  blue_green_update {
-    enabled = var.blue_green_update_enabled
-  }
-
   vpc_security_group_ids = [
     aws_security_group.this.id
   ]
@@ -80,14 +76,4 @@ resource "aws_security_group_rule" "access_from_vpc" {
   protocol    = "tcp"
 
   cidr_blocks = [var.vpc.cidr]
-}
-
-resource "aws_security_group_rule" "allow_all_out" {
-  type              = "egress"
-  to_port           = 0
-  protocol          = "-1"
-  from_port         = 0
-  security_group_id = aws_security_group.this.id
-  cidr_blocks       = ["0.0.0.0/0"]
-  ipv6_cidr_blocks  = ["::/0"]
 }
