8bce15d6c526bf642718a3c4c506cedecfcbd80d
Akhil Anil <140489780+akhil-redacre@users.noreply.github.com>
2024-01-02 18:27:39 +0530

    fix: removed max_allocated_storage_factor as we move to add max_storage directly

--- aws/rds/variables.tf
+++ aws/rds/variables.tf
@@ -134,12 +134,6 @@ variable "create_monitoring_role" {
   default = false
 }
 
-variable "max_allocated_storage_factor" {
-  type    = number
-  default = 2 # by deafult twice the amount of disk for the db instance
-  nullable = false
-}
-
 variable "max_allocated_storage" {
   type    = number
   nullable = false
