0adcdb27b83c2d37274569d18245b272ff294f7a
elsebasan <sebastian.sanchez@redacreltd.com>
2024-02-05 08:16:26 -0300

    chore(apps/frontends): add cloudflare_ruleset cahce_rule

--- apps/frontends/cache_rules.tf
+++ apps/frontends/cache_rules.tf
@@ -1,10 +1,83 @@
 
-resource "local_file" "example" {
+#resource "local_file" "example" {
+#  for_each = {
+#    for partition, data in var.partitions : partition => data 
+#    if contains(keys(data), "api")
+#  }
+#
+#  content  = each.value.api
+#  filename = "${path.module}/folder/${each.key}"
+#}
+
+
+# Cache rule configuring cache settings and defining custom cache keys
+resource "cloudflare_ruleset" "cache_rules_example" {
+  
   for_each = {
     for partition, data in var.partitions : partition => data 
     if contains(keys(data), "api")
   }
 
-  content  = each.value.api
-  filename = "${path.module}/folder/${each.key}"
+  zone_id     = data.cloudflare_zone.this[each.key].id
+  name        = "Set cache settings"
+  description = "Set cache settings for incoming requests"
+  kind        = "zone"
+  phase       = "http_request_cache_settings"
+
+  rules {
+    action = "set_cache_settings"
+    action_parameters {
+      edge_ttl {
+        mode    = "override_origin"
+        default = 60
+        status_code_ttl {
+          status_code = 200
+          value       = 50
+        }
+        status_code_ttl {
+          status_code_range {
+            from = 201
+            to   = 300
+          }
+          value = 30
+        }
+      }
+      browser_ttl {
+        mode = "respect_origin"
+      }
+      serve_stale {
+        disable_stale_while_updating = true
+      }
+      respect_strong_etags = true
+      cache_key {
+        ignore_query_strings_order = false
+        cache_deception_armor      = true
+        custom_key {
+          query_string {
+            exclude = ["*"]
+          }
+          header {
+            include        = ["habc", "hdef"]
+            check_presence = ["habc_t", "hdef_t"]
+            exclude_origin = true
+          }
+          cookie {
+            include        = ["cabc", "cdef"]
+            check_presence = ["cabc_t", "cdef_t"]
+          }
+          user {
+            device_type = true
+            geo         = false
+          }
+          host {
+            resolved = true
+          }
+        }
+      }
+      origin_error_page_passthru = false
+    }
+    expression  = "(http.host eq \"example.net\")"
+    description = "Set cache settings and custom cache key for example.net"
+    enabled     = true
+  }
 }
