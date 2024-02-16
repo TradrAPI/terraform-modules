ff8dbbfc10698c1d8b358b63fa9cc4e495b44b48
Leonardo Prado <leonardo.prado@redacreltd.com>
2024-02-16 16:09:43 +0100

    fix: Rm wrong code

--- postgres/dbs/databases.tf
+++
@@ -1,6 +0,0 @@
-resource "postgresql_database" "databases" {
-  for_each = toset(var.databases)
-
-  name  = each.value
-  owner = var.owner
-}
--- postgres/dbs/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "users" {
-  value = {
-    for name, role in var.roles:
-    name => random_password.all[name].result
-    if role.login
-  }
-}
