ee079405f2ca09f4554f0a28d5e3a9f959ef85b0
o-leolleo <leonardo.prado@redacreltd.com>
2024-02-26 14:53:13 +0100

    fix: Required providers version (#25)

--- aws/cert/version.tf
+++ aws/cert/version.tf
@@ -3,12 +3,10 @@ terraform {
   required_providers {
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "~> 3.0"
     }
 
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
     }
   }
 }
