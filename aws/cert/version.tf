630584546d2c50f238e647b6b410469788a79e79
o-leolleo <leonardo.prado@redacreltd.com>
2024-02-26 13:42:07 +0100

    feat: Adds cert module (#22)

---
+++ aws/cert/version.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 3.0"
+    }
+
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
