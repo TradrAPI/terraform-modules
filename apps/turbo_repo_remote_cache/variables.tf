5e231f3a3ca46220d705f0b600903626375e49cc
Leonardo Prado <leonardo.prado@redacreltd.com>
2024-06-05 15:53:18 +0200

    chore: Remove unused variable

--- apps/turbo_repo_remote_cache/variables.tf
+++ apps/turbo_repo_remote_cache/variables.tf
@@ -2,11 +2,6 @@ variable "bucket_name" {
   type = string
 }
 
-variable "role_name" {
-  type    = string
-  default = "TurborepoRemoteCache"
-}
-
 variable "lambda_name" {
   type    = string
   default = "turborepo-cache-lambda"
