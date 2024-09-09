e3c582fcbe308ed93b0c58cf066f4e51225fd950
Akhil Anil <140489780+akhil-redacre@users.noreply.github.com>
2024-09-09 15:42:47 +0530

    fix(mongodb): Resolve undetermined count issue in random_password resource (#82)
    * fix(mongodb-module): undeterminted count error
    
    * fix(mongodb): resolve count issue in random_password resource

--- mongodbatlas/user/main.tf
+++ mongodbatlas/user/main.tf
@@ -32,7 +32,7 @@ resource "mongodbatlas_database_user" "user" {
 }
 
 resource "random_password" "password" {
-  count   = var.external_password == null ? 1 : 0
+  count   = var.external_password != null ? 0 : 1 # Use the password based on the condition
   length  = 24
   special = false
 }
