0c1b49dcc6d9c20e01a85bfcfb1927b9643206bf
Kurt Portelli <kurt.portelli@redacreltd.com>
2024-11-11 13:20:27 +0100

    feat(ec2): Add defaults to ebs inside ec2

--- aws/ec2/ebs.tf
+++ aws/ec2/ebs.tf
@@ -2,10 +2,13 @@ resource "aws_ebs_volume" "this" {
   count = var.ebs != null ? 1 : 0
 
   size              = var.ebs.size
-  tags              = var.ebs.tags
-  availability_zone = var.ebs.az
-  type              = var.ebs.type
-  iops              = var.ebs.iops
+  availability_zone = try(var.ebs.az, var.availability_zone)
+  type              = try(var.ebs.type, null)
+  iops              = try(var.ebs.iops, null)
+
+  tags = {
+    Name = "${var.name}-ebs${var.ebs.size}g"
+  }
 }
 
 resource "aws_volume_attachment" "this" {
@@ -14,7 +17,7 @@ resource "aws_volume_attachment" "this" {
   volume_id   = aws_ebs_volume.this[0].id
   instance_id = aws_instance.this.id
 
-  device_name = var.ebs.device_name
+  device_name = try(var.ebs.device_name, "xvdf")
 
   stop_instance_before_detaching = true
 }
