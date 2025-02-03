2209fb7a0a7f26dec47e503de7c6a0e5149793e6
Leonardo Prado <leonardo.prado@redacreltd.com>
2025-02-03 12:26:56 +0100

    feat(network)!: Makes routes v2 the default

--- aws/network/main.tf
+++ aws/network/main.tf
@@ -143,87 +143,13 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "this" {
   }
 }
 
-
-/* route tables */
-resource "aws_route_table" "private" {
-  count = !var.remove_all_private_route_tables_v1 ? length(var.az_zones) : 0
-
-  vpc_id = aws_vpc.default.id
-
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = element(aws_nat_gateway.default.*.id, count.index)
-  }
-
-  dynamic "route" {
-    for_each = distinct(concat(
-      var.extra_private_routes,
-      local.extra_tgw_routes,
-      try(var.extra_private_routes_per_az[var.az_zones[count.index]], []),
-    ))
-
-    content {
-      cidr_block                = route.value["cidr_block"]
-      vpc_peering_connection_id = try(route.value["vpc_peering_connection_id"], null)
-      network_interface_id      = try(route.value["network_interface_id"], null)
-      transit_gateway_id        = try(route.value["transit_gateway_id"], null)
-    }
-  }
-
-
-  tags = {
-    Name        = "${var.name}-Private-${var.az_zones[count.index]}-routetable"
-    Description = "Route table Target to Nat Gateway for ${var.name}"
-    Created-By  = "DevOps-Terraform"
-    Environment = var.deployment_env
-  }
-
-  depends_on = [aws_nat_gateway.default]
-}
-
-resource "aws_route_table" "public" {
-  count  = !var.remove_all_public_route_tables_v1 ? length(var.az_zones) : 0
-  vpc_id = aws_vpc.default.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.default.id
-  }
-
-  dynamic "route" {
-    for_each = distinct(concat(
-      var.extra_public_routes,
-      var.enable_tgw_routes_in_public_subnets ? local.extra_tgw_routes : [],
-    ))
-
-    content {
-      cidr_block                = route.value["cidr_block"]
-      vpc_peering_connection_id = try(route.value["vpc_peering_connection_id"], null)
-      network_interface_id      = try(route.value["network_interface_id"], null)
-      transit_gateway_id        = try(route.value["transit_gateway_id"], null)
-    }
-  }
-
-  tags = {
-    Name        = "${var.name}-Public-${var.az_zones[count.index]}-routetable"
-    Description = "Route table Target to Internet Gateway for ${var.name}"
-    Created-By  = "DevOps-Terraform"
-    Environment = var.deployment_env
-  }
-  depends_on = [aws_internet_gateway.default]
-}
-
 /* Subnets Assciation for Public and Private */
 resource "aws_route_table_association" "private" {
   count = length(var.az_zones)
 
   subnet_id = aws_subnet.private[count.index].id
 
-  route_table_id = (
-    count.index < var.associate_private_route_table_v2
-    ? aws_route_table.private_v2[count.index].id
-    : aws_route_table.private[count.index].id
-  )
+  route_table_id = aws_route_table.private[count.index].id
 
   depends_on = [
     aws_subnet.private,
@@ -236,11 +162,7 @@ resource "aws_route_table_association" "public" {
 
   subnet_id = aws_subnet.public[count.index].id
 
-  route_table_id = (
-    count.index < var.associate_public_route_table_v2
-    ? aws_route_table.public_v2[count.index].id
-    : aws_route_table.public[count.index].id
-  )
+  route_table_id = aws_route_table.public[count.index].id
 
   depends_on = [
     aws_subnet.public,
