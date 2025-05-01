328ca7e3a9a815e01c66181617e8b79b5b9ae97b
Leonardo Prado <leonardo.prado@redacreltd.com>
2025-05-01 11:15:18 +0200

    chore(kafka): Improve parameters descriptions

---
+++ aws/kafka/variables.tf
@@ -0,0 +1,118 @@
+
+variable "amazon_s3_sink_connector_url" {
+  description = "Amazon S3 Sink Connector URL"
+  type        = string
+  default     = "https://d2p6pa21dvn84.cloudfront.net/api/plugins/confluentinc/kafka-connect-s3/versions/10.5.2/confluentinc-kafka-connect-s3-10.5.2.zip"
+}
+
+variable "platform" {
+  description = "Platform name"
+  type        = string
+  default     = null
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = null
+}
+
+variable "cluster_name" {
+  description = "MSK cluster name"
+  type        = string
+  default     = null
+}
+
+
+variable "kafka_version" {
+  description = "Kafka version (See https://docs.aws.amazon.com/msk/latest/developerguide/supported-kafka-versions.html)"
+  type        = string
+  default     = "3.4.0"
+
+}
+
+
+variable "number_of_broker_nodes" {
+  description = "Number of broker nodes"
+  type        = number
+  default     = 3
+}
+
+variable "client_subnets" {
+  description = "Client VPC subnets"
+  type        = list(string)
+  default     = []
+
+}
+
+variable "msk_instance_type" {
+  description = "MSK instance type (See https://docs.aws.amazon.com/msk/latest/developerguide/broker-instance-sizes.html)"
+  type        = string
+  default     = "kafka.t3.small"
+
+}
+
+variable "ebs_volume_size" {
+  description = "EBS volume size (in GiB) for each broker node"
+  type        = number
+  default     = 10
+}
+
+
+variable "vpc_id" {
+  description = "ID of the VPC where the MSK cluster will be created"
+  type        = string
+  default     = null
+}
+
+
+
+
+variable "allowed_cidr_blocks" {
+  description = "Allowed CIDR blocks that can access the MSK cluster"
+  type        = list(string)
+  default     = []
+
+}
+
+
+variable "users" {
+  description = "MSK users names list (passwords are generated automatically)"
+  type        = list(string)
+  default     = []
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = null
+}
+
+variable "server_properties" {
+  description = <<-EOF
+    Server properties. You can specify any of the properties in https://docs.aws.amazon.com/msk/latest/developerguide/msk-configuration-properties.html".
+
+    Example:
+    ```
+      server_properties = <<-EOF2
+        auto.create.topics.enable=true
+        delete.topic.enable=true
+      EOF2
+    ```
+  EOF
+  type        = string
+  default     = null
+}
+
+variable "private_subnets" {
+  description = "Kafka connectors private subnets, usually the same as client subnets"
+  type        = list(string)
+  default     = []
+}
+
+variable "default_num_partitions" {
+  description = "Default number of partitions (>= number_of_broker_nodes is recommended)"
+  type        = number
+  default     = null
+  nullable    = true
+}
