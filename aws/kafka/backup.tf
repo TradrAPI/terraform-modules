0ea907d95d38c51136d8904153378a23786a56d8
Leonardo Prado <leonardo.prado@redacreltd.com>
2025-05-01 14:14:08 +0200

    chore(kafka): Rm backup moved blocks since recreating the connector

--- aws/kafka/backup.tf
+++ aws/kafka/backup.tf
@@ -42,51 +42,6 @@ module "msk_backup" {
   }
 }
 
-moved {
-  from = module.msk_s3_bkp
-  to   = module.msk_backup.module.backup_bucket[0]
-}
-
-moved {
-  from = aws_security_group.s3_sink_connector
-  to   = module.msk_backup.aws_security_group.s3_sink_connector
-}
-
-moved {
-  from = aws_cloudwatch_log_group.s3_sink_connector
-  to   = module.msk_backup.aws_cloudwatch_log_group.s3_sink_connector
-}
-
-moved {
-  from = aws_security_group_rule.s3_sink_connector_outbound
-  to   = module.msk_backup.aws_security_group_rule.s3_sink_connector_outbound
-}
-
-moved {
-  from = aws_s3_bucket_server_side_encryption_configuration.msk_s3_bkp
-  to   = module.msk_backup.aws_s3_bucket_server_side_encryption_configuration.backup_bucket[0]
-}
-
-moved {
-  from = aws_iam_role.msk_s3_bkp
-  to   = module.msk_backup.aws_iam_role.backup
-}
-
-moved {
-  from = aws_iam_policy.msk_s3_bkp
-  to   = module.msk_backup.aws_iam_policy.backup
-}
-
-moved {
-  from = aws_iam_role_policy_attachment.msk_s3_bkp
-  to   = module.msk_backup.aws_iam_role_policy_attachment.backup
-}
-
-moved {
-  from = aws_mskconnect_connector.backup_msk_to_s3
-  to   = module.msk_backup.aws_mskconnect_connector.backup_msk_to_s3
-}
-
 module "mskconnect_plugins" {
   source = "../kafka_plugins"
 
@@ -94,24 +49,4 @@ module "mskconnect_plugins" {
   create_bucket = true
 
   plugins = {}
-}
-
-moved {
-  from = module.mskconnect_custom_plugins
-  to   = module.mskconnect_plugins.module.plugins_bucket[0]
-}
-
-moved {
-  from = aws_s3_object.plugins
-  to   = module.mskconnect_plugins.aws_s3_object.plugins
-}
-
-moved {
-  from = aws_mskconnect_custom_plugin.plugins
-  to   = module.mskconnect_plugins.aws_mskconnect_custom_plugin.plugins
-}
-
-moved {
-  from = terraform_data.plugins
-  to   = module.mskconnect_plugins.terraform_data.plugins
 }
