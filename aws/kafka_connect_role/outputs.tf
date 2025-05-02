dae56e5d8f07b7cce28b185c0dc6276d11005e43
Leonardo Prado <leonardo.prado@redacreltd.com>
2025-05-02 15:14:37 +0200

    feat(kafka_connect_role): Outputs the role arn

--- aws/kafka_connect_role/outputs.tf
+++ aws/kafka_connect_role/outputs.tf
@@ -2,3 +2,8 @@ output "role_name" {
   description = "The IAM role nae for Kafka Connect."
   value       = aws_iam_role.this.name
 }
+
+output "role_arn" {
+  description = "The ARN of the IAM role for Kafka Connect."
+  value       = aws_iam_role.this.arn
+}
