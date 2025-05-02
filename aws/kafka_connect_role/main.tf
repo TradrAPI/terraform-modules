8f129742309fb92ce2f33d29484787b52f63b447
Leonardo Prado <leonardo.prado@redacreltd.com>
2025-05-02 11:38:42 +0200

    feat(kafka_connect_role): Creates IAM Auth execution role for MSK connectors

---
+++ aws/kafka_connect_role/main.tf
@@ -0,0 +1,91 @@
+resource "aws_iam_role_policy_attachment" "this" {
+  role       = aws_iam_role.this.name
+  policy_arn = aws_iam_policy.kafka_iam_auth.arn
+}
+
+resource "aws_iam_role" "this" {
+  name = var.role_name
+
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "kafkaconnect.amazonaws.com"
+        },
+        "Action" : "sts:AssumeRole"
+        "Condition" : {
+          "StringEquals" : {
+            "aws:SourceAccount" : data.aws_caller_identity.this.account_id
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "kafka_iam_auth" {
+  name = replace(title("${var.role_name}-kafka-iam-auth"), "-", "")
+
+  description = "IAM policy for Kafka Connect to authenticate with MSK using IAM"
+
+  policy = jsondecode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "kafka-cluster:Connect",
+          "kafka-cluster:DescribeCluster"
+        ],
+        "Resource" : [
+          "arn:aws:kafka:${data.aws_region.this.name}:${data.aws_caller_identity.this.account_id}:cluster/${var.cluster_name}/${var.cluster_id}"
+        ]
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "kafka-cluster:ReadData",
+          "kafka-cluster:DescribeTopic"
+        ],
+        "Resource" : [
+          "arn:aws:kafka:${data.aws_region.this.name}:${data.aws_caller_identity.this.account_id}:topic/${var.cluster_name}/${var.cluster_id}/__amazon_msk_connect_read"
+        ]
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "kafka-cluster:WriteData",
+          "kafka-cluster:DescribeTopic"
+        ],
+        "Resource" : [
+          "arn:aws:kafka:${data.aws_region.this.name}:${data.aws_caller_identity.this.account_id}:topic/${var.cluster_name}/${var.cluster_id}/__amazon_msk_connect_write"
+        ]
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "kafka-cluster:CreateTopic",
+          "kafka-cluster:WriteData",
+          "kafka-cluster:ReadData",
+          "kafka-cluster:DescribeTopic"
+        ],
+        "Resource" : [
+          "arn:aws:kafka:${data.aws_region.this.name}:${data.aws_caller_identity.this.account_id}:topic/${var.cluster_name}/${var.cluster_id}/__amazon_msk_connect_*"
+        ]
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "kafka-cluster:AlterGroup",
+          "kafka-cluster:DescribeGroup"
+        ],
+        "Resource" : [
+          "arn:aws:kafka:${data.aws_region.this.name}:${data.aws_caller_identity.this.account_id}:group/${var.cluster_name}/${var.cluster_id}/__amazon_msk_connect_*",
+          "arn:aws:kafka:${data.aws_region.this.name}:${data.aws_caller_identity.this.account_id}:group/${var.cluster_name}/${var.cluster_id}/connect-*"
+        ]
+      }
+    ]
+  })
+}
